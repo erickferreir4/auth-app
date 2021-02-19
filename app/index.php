90a148a28623cf34b603947c47a6254920518c24
Erick Ferreira <erick.xferreira@gmail.com>
2021-02-19 14:55:48 -0300

    chore: add composer.json

--- app/index.php
+++ app/index.php
@@ -1,6 +1,9 @@
 <?php declare(strict_types=1);
 
 require __DIR__ . '/core/init.php';
+//require __DIR__ . '/vendor/autoload.php';
+
+//phpinfo();
 
 $app = new app\controller\AppController;
 $app->router();
